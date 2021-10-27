97c323cb3d480997d3dbebc235148541e8f441d2
qkrthdus605 <qkrthdus605@gmail.com>
2021-10-27 12:52:17 +0900

    추가 : 카테고리 구혐

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,8 @@
 // API를 요청하고 뉴스 데이터가 들어 있는 배열을 컴포넌트 배열로 변환해 렌더링
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -16,22 +17,46 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const sampleArticle = {
-  title: '제목',
-  description: '내용',
-  url: 'https://google.com',
-  urlToImage: 'https://via.placeholder.com/160',
-};
-
 const NewsList = () => {
+
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    //async를 사용하는 함수 따로 선언
+    const fetchData = async() => {
+      // 요청 중 loading -> true
+      setLoading(true);
+      try{
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=f5f4c537bfaa495ebcf98d1dd590b20c',
+        );
+        setArticles(response.data.articles);
+      }catch(e){
+        console.log(e);
+      }
+      // 요청이 끝나면 loading -> false
+      setLoading(false);
+    };
+    fetchData();
+  },[]);
+
+  // 대기 중일 때 
+  if(loading){
+    return <NewsListBlock>대기 중...</NewsListBlock>
+  }
+
+  // 아직 articles 값이 설정되지 않았을 떄
+  if(!articles){
+    return null;
+  }
+
+  // articles 값이 유효할 때
   return(
     <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {articles.map(article => (
+        <NewsItem key={article.url} article={article} />
+      ))}  
     </NewsListBlock>
   );
 };
